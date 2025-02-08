2f2526988f0af647652c9a12089b1bcaf6fad710
chiji1108 <shingo.pad@gmail.com>
2025-02-08 18:22:44 +0900

    Initial Commit

---
+++ PhotoStore/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PhotoStore
+//
+//  Created by 千々岩真吾 on 2025/02/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PhotoStore/PhotoStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoStoreApp.swift
+//  PhotoStore
+//
+//  Created by 千々岩真吾 on 2025/02/08.
+//
+
+import SwiftUI
+
+@main
+struct PhotoStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
